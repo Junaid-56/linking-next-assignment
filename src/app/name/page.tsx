76db7a12a755f46cbd9c5297b1e8ba07cd54f4be
junaid-56 <junaididrees1111@gmail.com>
2024-03-29 17:52:14 +0500

    linking assignment

---
+++ src/app/name/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function page1() {
+  return (
+    <div>
+      <h1>This is the Name Page</h1>
+      <br></br>
+      <br></br>
+      <Link href="/">click here to Go to HOMEPAGE</Link>
+    </div>
+  );
+}
